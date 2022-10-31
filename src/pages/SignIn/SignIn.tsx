206bb87cfd5c077f9422f10759ea5f70fa567cbf
jandersonvb <jandersonvb.dev@gmail.com>
2022-10-31 19:32:56 -0300

    feat: importing application logo

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
 
 import { Container } from './SignIn.styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
 };
 export { SignIn };
